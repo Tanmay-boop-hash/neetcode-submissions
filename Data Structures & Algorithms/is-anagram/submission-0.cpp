156ad726e065d16745b126c9327dc817cf73f274
Tanmay Sonkar <tanmaysonkar24@gmail.com>
2026-07-13 20:47:11 +0530

    Bulk sync: 3 submissions

---
+++ Data Structures & Algorithms/is-anagram/submission-0.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.size() != t.size()) return false;
+        int alpha[26]={0};
+
+        for(int i=0; i<s.size(); i++){
+            alpha[s[i] - 'a']++;
+            alpha[t[i] - 'a']--;
+        }
+        for(int i: alpha){
+            if(i != 0) return false;
+        }
+        return true;
+    }
+};
