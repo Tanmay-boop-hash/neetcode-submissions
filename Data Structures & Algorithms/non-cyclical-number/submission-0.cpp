6541eeae44e888261583db2e59a8b52ad402a010
Tanmay Sonkar <tanmaysonkar24@gmail.com>
2026-07-29 03:38:20 +0530

    Add: non-cyclical-number - submission-0

---
+++ Data Structures & Algorithms/non-cyclical-number/submission-0.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int sumDigits(int n){
+        int sum=0;
+        while(n != 0){
+            int d = n%10;
+            sum += d * d;
+            n /= 10;
+        }
+        return sum;
+    }
+    bool isHappy(int n) {
+        unordered_set<int> seen;
+        while(n != 1){
+            if(seen.count(n)) return false;
+            seen.insert(n);
+
+            n = sumDigits(n);
+        }
+        return true;
+    }
+};
